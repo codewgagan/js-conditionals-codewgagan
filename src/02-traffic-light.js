7543d3f7a04f0332092d73e1808f297f702d20aa
Gagan Gowda <122716450+codewgagan@users.noreply.github.com>
2026-03-10 20:57:39 +0530

    solved traffic-light

--- src/02-traffic-light.js
+++ src/02-traffic-light.js
@@ -23,4 +23,22 @@
  */
 export function getTrafficAction(color) {
   // Your code here
+  if (typeof color !== "string") {
+    return "INVALID SIGNAL";
+  }
+
+  let normalisedColor = color.toLowerCase();
+
+  switch (normalisedColor) {
+    case "green":
+      return "GO";
+    case "yellow":
+      return "SLOW DOWN";
+    case "red":
+      return "STOP";
+    case "flashing red":
+      return "STOP AND PROCEED WITH CAUTION";
+    default:
+      return "INVALID SIGNAL";
+  }
 }
