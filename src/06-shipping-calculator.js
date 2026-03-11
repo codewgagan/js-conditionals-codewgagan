7f3198149942199d8541ffeedea0b9b51c5bbc20
Gagan Gowda <122716450+codewgagan@users.noreply.github.com>
2026-03-11 17:28:10 +0530

    Solved 06-Shipping-calculator

--- src/06-shipping-calculator.js
+++ src/06-shipping-calculator.js
@@ -30,4 +30,46 @@
  */
 export function calculateShipping(weight, country, orderTotal) {
   // Your code here
+  /*
+   *validate
+   *free shipping
+   *check country
+   *check weight
+   *return cost
+   */
+  /**
+   * if weight <= 0 → -1
+if orderTotal < 0 → -1
+
+if country === "US" AND orderTotal > 50 → 0
+if country !== "US" AND orderTotal > 100 → 0
+
+if country === "US"
+    if weight <= 1 → 5
+    else if weight <= 5 → 10
+    else → 15
+else
+    if weight <= 1 → 15
+    else if weight <= 5 → 25
+    else → 40
+   */
+  if ( weight <= 0) return -1;
+  if ( orderTotal < 0) return -1;
+
+  // free shipping
+  if (country === "US" && orderTotal > 50) {
+    return 0;
+  } else if (country !== "US" && orderTotal > 100) return 0;
+
+  // Domestic
+  if(country === 'US'){
+    if(weight<=1) return 5;
+    else if(weight <=5) return 10;
+    else return 15;
+  }else{
+    if(weight<=1)return 15;
+    else if(weight<=5)return 25
+    else return 15
+  }
+
 }
