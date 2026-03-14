b3998d8032b966a6e1f6818c25f1420cb0bf4770
Gagan Gowda <122716450+codewgagan@users.noreply.github.com>
2026-03-14 23:51:46 +0530

    solved 10-tip-calculator

--- src/10-tip-calculator.js
+++ src/10-tip-calculator.js
@@ -31,4 +31,20 @@
  */
 export function calculateTip(billAmount, serviceRating) {
   // Your code here
+  if (billAmount <= 0) return null;
+  if (!Number.isInteger(serviceRating) || serviceRating < 1 || serviceRating>5) {return null};
+  let tipPercentage;
+  if (serviceRating === 1) tipPercentage = 5;
+  else if (serviceRating === 2) tipPercentage = 10;
+  else if (serviceRating === 3) tipPercentage = 15;
+  else if (serviceRating === 4) tipPercentage = 20;
+  else if (serviceRating === 5) tipPercentage = 25;
+
+  let tipAmount = (billAmount * tipPercentage) / 100;
+  let totalAmount = billAmount + tipAmount;
+
+  tipAmount = Number(tipAmount.toFixed(2));
+  totalAmount = Number(totalAmount.toFixed(2));
+
+  return { tipPercentage,tipAmount, totalAmount };
 }
